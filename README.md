c2bf93d502aa9e2c2c63a6890a4d2d7b86ac8785
yllekz <28446638+yllekz@users.noreply.github.com>
2022-02-08 23:04:02 -0500

    Many updates to PS script -New second hand options + wavs -Second hand launches in parallel now -Fix param types for hours -Move global var definitions out of while loop -Update readme

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# talking-clock, slightly improved version
+# talking-clock, next-generation
 
+## Grandfather/wall clock emulator script
+
+### From here on out, I'm intending to focus on the PowerShell version of this script due to scripting language preference + cross-compatibility. The bash script will remain but I do not plan to backport new features to it.
+
+**Changes/Improvements from the original/other forks**
+* Make the script's default actions nonverbal chimes.
+* Easy-to-read instructions on how to install/use
+* Bugfix: Bash script didn't correctly point to the wav files
+* Integrate recordings of the [UNH Thompson Hall Clock](https://www.youtube.com/watch?v=pmfMDvX646g) chimes
+    * bang.wav replaced (original is bang-bak.wav)
+* Plays chimes based on [Westminster Chimes cadence](https://en.wikipedia.org/wiki/Westminster_Quarters)
+* Integrate recordings of various Sessions/Seth Thomas wall clock sounds
+* Second hand sounds
+
+**Installation/Use**
 ```bash
 talking-clock:
 -f [n]
@@ -9,15 +24,6 @@ talking-clock:
     stops clock
 ```
 
-**Changes/Improvements from the original/other forks**
-* Make the script's default actions nonverbal chimes.
-* Easy-to-read instructions on how to install/use
-* Bugfix: Script didn't correctly point to the wav files
-* Integrate recordings of the [UNH Thompson Hall Clock](https://www.youtube.com/watch?v=pmfMDvX646g) chimes
-    * bang.wav replaced (original is bang-bak.wav)
-* Plays chimes based on [Westminster Chimes cadence](https://en.wikipedia.org/wiki/Westminster_Quarters)
-
-**Installation/Use**
 ```
 #Linux:
 # Pull down the code to your local system:
@@ -26,7 +32,7 @@ sudo git clone https://github.com/yllekz/talking-clock /usr/share/talking-clock
 # Make script executable + run it:
 
 # Linux - If you like bash:
-`sudo chmod +x /usr/share/talking-clock/source/talking-clock`
+sudo chmod +x /usr/share/talking-clock/source/talking-clock
 cd /usr/share/talking-clock/source && ./talking-clock
 
 # Linux - If you like PowerShell:
@@ -44,13 +50,13 @@ cd /usr/share/talking-clock/source && pwsh -file ./talking-clock-ps.ps1
 ```
 
 **Compatibility**
-* Tested and working on Linux Mint Cinnamon 20.2
+* Tested and working on Linux Mint Cinnamon 20.2 [bash and PowerShell]
     * Requires [PowerShell](https://github.com/powershell/powershell/) if using the PowerShell script
-* Tested with inconclusive results on Windows 10 via Ubuntu on Windows Subsystem for Linux (need to run this as a prereq: `sudo apt install alsa-utils`)
+* PowerShell script tested and working on Windows 10 via [PowerShell 7](https://github.com/powershell/powershell/).
+* Bash script tested with inconclusive results on Windows 10 via Ubuntu on Windows Subsystem for Linux (need to run this as a prereq: `sudo apt install alsa-utils`)
     * While the script itself will execute just fine, I ran into issues with `aplay` not finding the sound card. [This article](https://research.wmz.ninja/articles/2017/11/setting-up-wsl-with-graphics-and-audio.html) might help but it's quite a setup that isn't really worth it.
     * As a workaround, I ported of the Bash script to PowerShell, using .NET invocations to play the chimes. Check out the "talking-clock-ps.ps1" file if you want to use this on Windows, as this will work. It's not a one-to-one port, but the fundamentals are there.
-* PowerShell script now supports Linux as well if you'd rather use that. macOS compatibility will arrive some time in the future.
-* Not yet tested on macOS
+* macOS compatibility will arrive some time in the future.
 
 **Notes**
 * By default, the Bash script will run in the background and write the time (every 60 seconds) to console you invoked it on (if you left it open).
@@ -59,6 +65,7 @@ cd /usr/share/talking-clock/source && pwsh -file ./talking-clock-ps.ps1
 * If using the Bash script, to see if it's running: `ps -aux | grep talking`
     * Or open `htop` and search for `talking-clock` if `ps` does not show it.
 * If using the Bash script, to end it: ``killall talking-clock``
+* If using the PowerShell script on Linux, to end it, find the PID via `ps -aux` then use the `kill` command on its ID. Example: `kill 48204`
 
 **TODO:**
 * [from original repo] consider replacing *aplay* with *play* from *sox* package for volume control option.
--- source/talking-clock-ps.ps1
+++ source/talking-clock-ps.ps1
@@ -5,23 +5,23 @@
 # Usage:
 # pwsh -file /usr/share/talking-clock/source/talking-clock-ps.ps1
 # pwsh -file /usr/share/talking-clock/source/talking-clock-ps.ps1 -ChimeStartHour 8 -ChimeEndHour 19 -Frequency 60
-# pwsh -file /usr/share/talking-clock/source/talking-clock-ps.ps1 -ChimeStartHour 8 -ChimeEndHour 19 -Frequency 60 -SecondHand
+# pwsh -file /usr/share/talking-clock/source/talking-clock-ps.ps1 -ChimeStartHour 8 -ChimeEndHour 19 -Frequency 30 -SecondHand 1
 
 # Parameters:
 # [Optional] Chime Start Hour: What hour (24h) to start chiming. If no flag specified, default is 0800 hours
 # [Optional] Chime End Hour: What hour (24h) to end chiming. If no flag specified, default is 1900 hours
 # [Optional] Frequency: Frequency of chimes (example: 15, 30, 60). If no flag specified clock will play every 15 minutes and performa full chime at the full
-# [Optional] Second Hand: Play audible second hand ticks. Default is false.
+# [Optional] Second Hand: Play audible second hand ticks, with options for various sounds if not specified, it will be off. 0 = none [default] 1 = Sessions clock tick. 2 = Foyer clock tick. 3 = Seth Thomas clock tick 1. 4 = Seth Thomas clock tick 2.
 
 Param (
     [Parameter()]
-    [String]$ChimeStartHour,
+    [Int32]$ChimeStartHour,
     [Parameter()]
-    [String]$ChimeEndHour,
+    [Int32]$ChimeEndHour,
     [Parameter()]
     [Int32]$Frequency,
     [Parameter()]
-    [switch]$SecondHand
+    [Int32]$SecondHand
 )
 
 if(!$ChimeStartHour){
@@ -37,18 +37,62 @@ if(!$Frequency){
 }
 
 if(!$SecondHand){
-    $SecondHand = $false
+    $SecondHand = 0
 }
 
-write-warning "Chime hours defined as $($ChimeStartHour) to $($ChimeEndHour), with frequency of $($Frequency). Play seconds? $($SecondHand). Waiting till the top of the minute to begin."
+write-warning "Chime hours defined as $($ChimeStartHour) to $($ChimeEndHour), with frequency of $($Frequency). Second hand style: $($SecondHand). Waiting till the top of the minute to begin."
 
 while( (get-date).ToString("ss") -ne '00' ){
     #write-debug "Waiting until we're at 0 seconds in the minute before we begin"
     start-sleep 1 # To be nice to the CPU
 }
 
-$run = $true
+$run                = $true
+$BigChime           = "unhhourly.wav"
+$SmallChime         = "bang.wav"
+
+if($SecondHand -eq 0){
+    #n/a
+}elseif($SecondHand -eq 1){
+    $SecondHandTick = "sessionsclocktick.wav"
+}elseif($SecondHand -eq 2){
+    $SecondHandTick = "foyerclocktick.wav"
+}elseif($SecondHand -eq 3){
+    $SecondHandTick = "seththomasclocktick1.wav"
+}elseif($SecondHand -eq 4){
+    $SecondHandTick = "seththomasclocktick2.wav"
+}else{
+    write-error "Invalid second hand option."
+    exit
+}
+
+$BigChimePlayed     = $false
+$HourlyBongsPlayed  = $false
+$SmallChimePlayed   = $false
+
 Write-Warning "Script has begun"
+
+#region Play second hand ticks in a separate background job (but only if specified)
+if($SecondHand -ge 1){
+    Write-Warning "Activating second hand"
+    if($IsLinux -eq $true){
+        Start-Job{
+            while($using:run -eq $true){
+                aplay -q "$using:psscriptroot/sound/$($using:SecondHandTick)"
+            }
+        }|Out-Null
+    }elseif($IsMacOS -eq $true){
+        #TODO: TBD
+    }else{
+        #Windows
+        start-job{
+            while($using:run -eq $true){
+                (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SecondHandTick)").Play();
+            }
+        }|Out-Null
+    }
+}
+#endregion
 
 while($run -eq $true){
 
@@ -57,9 +101,6 @@ while($run -eq $true){
     $CurrentHour12H     = $date.ToString("hh")
     $CurrentMinute      = $date.ToString("mm")
     #$CurrentSecond      = $date.ToString("ss")
-    $BigChime           = "unhhourly.wav"
-    $SmallChime         = "bang.wav"
-    $SecondHandTick     = "sessionsclocktick.wav"
 
     #Write-Warning "$($currenthour24h):$($currentminute)"
 
@@ -67,34 +108,63 @@ while($run -eq $true){
 
         #Only chime if between certain hours
         if ( $CurrentHour24H -ge $ChimeStartHour -or $CurrentHour24H -le $ChimeEndHour ){
-            Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime"
-
-            if($IsLinux -eq $true){
-                aplay -q "$psscriptroot/sound/$($BigChime)"
-            }elseif($IsMacOS -eq $true){
-                #TODO: TBD
-            }else{
-                #Windows
-                (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($BigChime)").Play();
-                start-sleep 25 #To prevent overlapping over the next wav file
-            }
-
-            #play the chime x times based on the hour, shorten all but the last one
-            Write-Warning "Bong x$($CurrentHour12H)"
-            for($c=1; $c -le $CurrentHour12H; $c++){
-                if( ($c -ne $CurrentHour12H) ){
-                    #Play the bang abridged
-                    if($IsLinux -eq $true){
-                        aplay -q "$psscriptroot/sound/$($SmallChime)" -d 3
-                    }elseif($IsMacOS -eq $true){
-                        #TODO: TBD
-                    }else{
-                        #Windows
-                        (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
-                        start-sleep 3.5
-                    }
-
+            #region play the big hourly chime
+            if($BigChimePlayed -eq $false){
+                Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime"
+                if($IsLinux -eq $true){
+                    aplay -q "$psscriptroot/sound/$($BigChime)"
+                }elseif($IsMacOS -eq $true){
+                    #TODO: TBD
                 }else{
+                    #Windows
+                    (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($BigChime)").Play();
+                    start-sleep 25 #To prevent overlapping over the next wav file
+                }
+                $BigChimePlayed = $true
+            }
+            #endregion
+
+            #region play the chime x times based on the hour, shorten all but the last one
+            if($HourlyBongsPlayed -eq $false){
+                Write-Warning "Bong x$($CurrentHour12H)"
+                for($c=1; $c -le $CurrentHour12H; $c++){
+                    if( ($c -ne $CurrentHour12H) ){
+                        #Play the bang abridged
+                        if($IsLinux -eq $true){
+                            aplay -q "$psscriptroot/sound/$($SmallChime)" -d 3
+                        }elseif($IsMacOS -eq $true){
+                            #TODO: TBD
+                        }else{
+                            #Windows
+                            (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+                            start-sleep 3.5
+                        }
+
+                    }else{
+                        #Play the final bang unabridged
+                        if($IsLinux -eq $true){
+                            aplay -q "$psscriptroot/sound/$($SmallChime)"
+                        }elseif($IsMacOS -eq $true){
+                            #TODO: TBD
+                        }else{
+                            #Windows
+                            (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+                        }
+                    }
+                }
+                $HourlyBongsPlayed = $true
+            }
+            #endregion
+        }
+
+    }elseif( ($CurrentMinute -eq '15') ){
+
+        if($Frequency -le 15){
+            #region Only small chime if between certain hours
+            if ( $CurrentHour24H -ge $ChimeStartHour -or $CurrentHour24H -le $ChimeEndHour ){
+
+                if($SmallChimePlayed -eq $false){
+                    Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 15min"
                     #Play the bang unabridged
                     if($IsLinux -eq $true){
                         aplay -q "$psscriptroot/sound/$($SmallChime)"
@@ -104,93 +174,75 @@ while($run -eq $true){
                         #Windows
                         (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
                     }
+                    $SmallChimePlayed = $True
                 }
             }
-        }
+            #endregion
 
-        start-sleep 35 #To prevent it from going off again in the same minute
-
-    }elseif( ($CurrentMinute -eq '15') ){
-
-        if($Frequency -le 15){
-            #Only chime if between certain hours
-            if ( $CurrentHour24H -ge $ChimeStartHour -or $CurrentHour24H -le $ChimeEndHour ){
-                Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 15min"
-                #Play the bang unabridged
-                if($IsLinux -eq $true){
-                    aplay -q "$psscriptroot/sound/$($SmallChime)"
-                }elseif($IsMacOS -eq $true){
-                    #TODO: TBD
-                }else{
-                    #Windows
-                    (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
-                }
-            }
-
-            start-sleep 60 #To prevent it from going off again in the same minute
         }else{
             write-debug "Not playing 15min chime, frequency is set to be $($Frequency)"
-            #start-sleep 60 #To prevent console spam
+            #start-sleep (59-$CurrentSecond) #To prevent console spam
         }
 
     }elseif( ($CurrentMinute -eq '30') ){
 
         if($Frequency -le 30){
-            #Only chime if between certain hours
+            #region Only small chime if between certain hours
             if ( $CurrentHour24H -ge $ChimeStartHour -or $CurrentHour24H -le $ChimeEndHour ){
-                Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 30min"
-                #Play the bang unabridged
-                if($IsLinux -eq $true){
-                    aplay -q "$psscriptroot/sound/$($SmallChime)"
-                }elseif($IsMacOS -eq $true){
-                    #TODO: TBD
-                }else{
-                    #Windows
-                    (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+
+                if($SmallChimePlayed -eq $false){
+                    Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 30min"
+                    #Play the bang unabridged
+                    if($IsLinux -eq $true){
+                        aplay -q "$psscriptroot/sound/$($SmallChime)"
+                    }elseif($IsMacOS -eq $true){
+                        #TODO: TBD
+                    }else{
+                        #Windows
+                        (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+                    }
+                    $SmallChimePlayed = $True
                 }
             }
+            #endregion
 
-            start-sleep 60 #To prevent it from going off again in the same minute
         }else{
             Write-Debug "Not playing 30min chime, frequency is set to be $($Frequency)"
-            #start-sleep 60 #To prevent console spam
+            #start-sleep (59-$CurrentSecond) #To prevent console spam
         }
 
     }elseif( ($CurrentMinute -eq '45') ){
 
-        if($Frequency -le 30){
-            #Only chime if between certain hours
+        if($Frequency -le 45){
+            #region Only chime if between certain hours
             if ( $CurrentHour24H -ge $ChimeStartHour -or $CurrentHour24H -le $ChimeEndHour ){
-                Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 45min"
-                #Play the bang unabridged
-                if($IsLinux -eq $true){
-                    aplay -q "$psscriptroot/sound/$($SmallChime)"
-                }elseif($IsMacOS -eq $true){
-                    #TODO: TBD
-                }else{
-                    #Windows
-                    (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+
+                if($SmallChimePlayed -eq $false){
+                    Write-Warning "$($CurrentHour24H):$($CurrentMinute) Chime 45min"
+                    #Play the bang unabridged
+                    if($IsLinux -eq $true){
+                        aplay -q "$psscriptroot/sound/$($SmallChime)"
+                    }elseif($IsMacOS -eq $true){
+                        #TODO: TBD
+                    }else{
+                        #Windows
+                        (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SmallChime)").Play();
+                    }
+                    $SmallChimePlayed = $true
                 }
             }
+            #endregion
 
-            start-sleep 60 #To prevent it from going off again in the same minute
         }else{
             Write-Debug "Not playing 45min chime, frequency is set to be $($Frequency)"
-            #start-sleep 60 #To prevent console spam
         }
 
     }else{
-        #Play second hand ticks (but only if specified)
-        if($SecondHand -eq $true){
-            if($IsLinux -eq $true){
-                aplay -q "$psscriptroot/sound/$($SecondHandTick)"
-            }elseif($IsMacOS -eq $true){
-                #TODO: TBD
-            }else{
-                #Windows
-                (New-Object Media.SoundPlayer "$($psscriptroot)\sound\$($SecondHandTick)").Play();
-            }
-        }
+        # (current minute does not trigger any chimes)
+        #Reset chime flag
+        $BigChimePlayed = $false
+        $SmallChimePlayed = $false
+        $HourlyBongsPlayed = $false
     }
 
     start-sleep 1 # To be nice to the CPU
